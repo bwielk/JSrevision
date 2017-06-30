5b8ede0c0d86cc4c1143d102a55e74a44632e452
bwielk <bwielk@gmail.com>
2017-06-30 06:36:10 +0200

    Revision 2

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello")
+console.log("Whazzz up")
+
+
+var name  = "John";
+console.log(name);
+
+var lastName = "Smith";
+
+console.log(name + lastName);
+
+var age = 8;
+var age1 = 9;
+var age2 = 10;
+
+console.log(age + age1 - age2);
+
+var adult = true;
+console.log(adult);
