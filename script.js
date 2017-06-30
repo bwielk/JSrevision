8826b4686bd8b3ff9b4cbc48811525d71c92c36f
bwielk <bwielk@gmail.com>
2017-06-30 10:45:42 +0200

    Revision 3

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-console.log("Hello")
-console.log("Whazzz up")
+//console.log("Hello")
+//console.log("Whazzz up")
 
 
 var name  = "John";
-console.log(name);
+//console.log(name);
 
 var lastName = "Smith";
 
-console.log(name + lastName);
+//console.log(name + lastName);
 
 var age = 8;
 var age1 = 9;
 var age2 = 10;
 
-console.log(age + age1 - age2);
+//console.log(age + age1 - age2);
 
 var adult = true;
-console.log(adult);
+//console.log(adult);
+
+//variables2
+
+console.log(name + age2);
+console.log(age + lastName);
+
+var job, isMarried;
+
+job = "doctor";
+isMarried = true;
+
+console.log(isMarried, job, age2+age1);
+
+var secondName = prompt("What is the last Name?");
+console.log(secondName);
